4ecef8ebd9184761905bb0d685d222d927809784
Hamza Meknassi <elmeknassi.h@gmail.com>
2020-03-28 02:49:18 +0100

    Added Bulma and a Register component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: RegisterComponent } // Route '' to main component and use directive CanActivate
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
